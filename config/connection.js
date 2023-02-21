52c938413cdd5354bcbe3bb074e30eb37611c284
linhklatt <112294288+linhklatt@users.noreply.github.com>
2023-02-20 21:24:31 -0800

    show message when connects to MongoDB

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/socialMedia", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/socialMedia", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!!!");
+  });
 
 module.exports = mongoose.connection;
