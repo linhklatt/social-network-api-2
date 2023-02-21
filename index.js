93e04e369157fe9fe4fa7f0eadd52bb4a32a5d54
linhklatt <112294288+linhklatt@users.noreply.github.com>
2023-02-21 11:24:59 -0800

    added utils folder for the format date

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connection = require("./config/connection");
-// const routes = require("./routes");
+const apiRoutes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
